19f406cda984b7198f9f8d9c67aff8a087e57c12
fikrifahruroji <fikrifahruroji@live.com>
2016-12-15 12:52:13 +0700

    Fix bug

--- app/src/main/java/id/fikri/testgson/MainActivity.java
+++ app/src/main/java/id/fikri/testgson/MainActivity.java
@@ -3,7 +3,6 @@ package id.fikri.testgson;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,33 +22,26 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
     Button btn_request;
     TextView lbl_http_connection;
     HttpURLConnection connection = null;
     BufferedReader reader = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_request = (Button) findViewById(R.id.btn_request);
-        lbl_http_connection = (TextView) findViewById(R.id.lbl_http_connection);
-
+        btn_request = (Button)findViewById(R.id.btn_request);
+        lbl_http_connection = (TextView)findViewById(R.id.lbl_http_connection);
         //button click will trigger http connection
         btn_request.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //localhost or 127.0.0.1 , is refer to emulator device it self
                 //use 10.0.2.2, for access local server
-                new ApiConnect().execute("https://private-7bb04d-signandlogin.apiary-mock.com/users/1");
+                new ApiConnect().execute("https://private-7bb04d-signandlogin.apiary-mock.com/users");
             }
         });
     }
-
-    //this method for handle http connection
-
     public String get_data(String url_target) {
         String line = "";
         try {
@@ -69,16 +61,15 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (connection != null) connection.disconnect();
+            if(connection != null) connection.disconnect();
             try {
-                if (reader != null) reader.close();
+                if(reader != null) reader.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         return null;
     }
-
     //this method for handel json parse
     public void process_json(String json_str) throws JSONException {
         try {
@@ -87,29 +78,38 @@ public class MainActivity extends AppCompatActivity {
             JSONObject api_json = new JSONObject(json_str);
             JSONArray users = api_json.getJSONArray("users");
             Gson gson_user = new Gson();
-            for (int i = 0; i < users.length(); i++) {
+            for(int i=0; i<users.length(); i++) {
                 JSONObject user = users.getJSONObject(i);
                 User model_user = gson_user.fromJson(String.valueOf(user), User.class);
-                Log.e("", "id : "+model_user.getId());
                 lbl_http_connection.append(
-                        "Id = " + model_user.getId() + System.getProperty("line.separator") +
-                                "Email = " + model_user.getEmail() + System.getProperty("line.separator") +
-                                "Password = " + model_user.getPassword() + System.getProperty("line.separator")
+                        "Id = " + model_user.getId() +
+                                System.getProperty("line.separator") +
+                                "Email = " + model_user.getEmail() +
+                                System.getProperty("line.separator") // +
+//                                "Password = " + model_user.getPassword()
+//                                System.getProperty("line.separator") +
+//                                "Token Auth = " + model_user.getToken_auth +
+//                                System.getProperty("line.separator") +
+//                                "Created at = " + model_user.getCreated_at +
+//                                System.getProperty("line.separator") +
+//                                "Updated at = " + model_user.getUpdated_at +
+//                                System.getProperty("line.separator") +
+//                                System.getProperty("line.separator")
                 );
             }
         } catch (JSONException e) {
+
             e.printStackTrace();
         }
     }
-
     //asynctask method will process http connection in background
     //http connection will not working in UI thread
+
     class ApiConnect extends AsyncTask<String, String, String> {
         @Override
         protected String doInBackground(String... params) {
             return get_data(params[0]);
         }
-
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
